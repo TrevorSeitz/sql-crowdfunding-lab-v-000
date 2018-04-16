0494effb11fa0c652cb4911768d789c987425df5
Trevor Seitz <trevinla@gmail.com>
2018-04-16 20:02:48 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -16,7 +16,7 @@ end
 
 def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_name
   "SELECT users.name, pledges.amount
-      FROM users
+      FROM pledges
       INNER JOIN pledges
       ON users.id = user_id
       GROUP BY users.name;"
