f2f751102063fb0a18742d6d10a695310a583f1b
Trevor Seitz <trevinla@gmail.com>
2018-04-16 21:06:52 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -34,7 +34,9 @@ end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_amount_and_users_name
 "Select users.name, SUM(pledges.amount),
-FROM projects"
+FROM projects
+ON projects.id = pledges.project.id
+GROUP BY users.nameORDER BY SUM(pledges.amount) ASC, users.name ASC"
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
